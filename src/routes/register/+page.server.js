bf2e2287e3ec2f14068ae79ad5448b776d3961bf
Martin Cajka <martin.cajka125@gmail.com>
2025-01-31 10:33:35 +0100

    add registration action to handle user sign-up

---
+++ src/routes/register/+page.server.js
@@ -0,0 +1,46 @@
+import { API_BASE_URL } from '$env/static/private';
+
+
+export const actions ={
+    
+    default: async ( {request}) => {
+        const formData = await request.formData();
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirmPassword');
+
+        console.log('API url:', API_BASE_URL);
+
+        try{
+        const response = await fetch(`${API_BASE_URL}/api/auth/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password, confirmPassword})
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.error(
+                "Registration failed:",
+                response.status,
+                data.detail || "Unknown error"
+              );
+            return {
+                success: false,
+                message: data.detail || "Registration failed. Please try again.",
+                email: email
+        }
+    }
+
+        return {
+            success: true,
+            message: data.message};
+    } catch (error) {
+        console.error("An error occurred", error);
+        return {message: "An error occurred"};
+    }
+}
+}
