51904480c2c66a1b0193228edb5049c596ba90bf
Martin Cajka <martin.cajka125@gmail.com>
2025-02-04 15:17:16 +0100

    add new icon components and styles for Delete, Edit, and Info

---
+++ src/components/icons/Delete.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "$lib/styles/icons.css";
+  let { iconStyle } = $props();
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  class={iconStyle}
+  fill="currentColor"
+>
+  <path
+    d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
+  />
+</svg>
